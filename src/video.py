6cdf6749561dc36fabaebed24f20b0e217eb1ff6
Shobhit Maheshwari <shmahesh@ucsc.edu>
2016-05-19 15:12:16 -0700

    morph video done

--- src/video.py
+++ src/video.py
@@ -4,6 +4,7 @@ import scipy.misc
 import matplotlib
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import operator
 
 
 def ani_frame(name, images):
@@ -25,8 +26,8 @@ def ani_frame(name, images):
     update_img.index = 0
     #legend(loc=0)
     ani = animation.FuncAnimation(fig,update_img,len(images) - 1)
-    writer = animation.writers['ffmpeg'](fps=2)
-    ani.save(name, writer=writer)
+    writer = animation.writers['ffmpeg'](fps=5)
+    ani.save('pics/'+name, writer=writer)
     return ani
 
 
@@ -80,51 +81,90 @@ def get_max_entropy_images(images):
         for y in range(x+1, len(images)):
             ent = get_entropy(images[x] - images[y])
             if(ent > max):
-                print(ent, x, y)
                 max = ent
                 max_x = x
                 max_y = y
     return max_x, max_y
 
 
-def test():
-    a = [
-        [1, 1, 0, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ]
-    b = [
-        [1, 0, 0, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [1, 0, 0, 1]
-    ]
-    images = [np.array(a),np.array(b)]
-    max = -1
-    max_x = 0
-    max_y = 0
-    for x in range(0, len(images)):
-        for y in range(x + 1, len(images)):
-            ent = get_entropy(images[x] - images[y])
-            if (ent > max):
-                print(ent, x, y)
-                max = ent
-                max_x = x
-                max_y = y
-    return x, y
+def get_value(arr, x, y):
+    if x < 0 or y < 0:
+        return 0
+    if(x >= len(arr)):
+        return 0
+    if(y >= len(arr[x])):
+        return 0
+    if(arr[x][y]):
+        return 1
+    else:
+        return 0
+
+
+def recompute_s(image, S, x, y):
+    S[(x, y)] = get_value(image, x - 1, y - 1) + get_value(image, x - 1, y) + get_value(image, x - 1, y + 1) \
+                + get_value(image, x, y - 1) + get_value(image, x, y) + get_value(image, x, y + 1) \
+                + get_value(image, x + 1, y - 1) + get_value(image, x + 1, y) + get_value(image, x + 1, y + 1)
+
+def recompute_s_all(image, d, S):
+    X = d[0]
+    Y = d[1]
+    if ((X - 1, Y - 1) in S):
+        recompute_s(image, S, X - 1, Y - 1)
+    if ((X - 1, Y) in S):
+        recompute_s(image, S, X - 1, Y)
+    if ((X - 1, Y + 1) in S):
+        recompute_s(image, S, X - 1, Y + 1)
+
+    if ((X, Y - 1) in S):
+        recompute_s(image, S, X, Y - 1)
+    if ((X, Y + 1) in S):
+        recompute_s(image, S, X, Y + 1)
+
+    if ((X + 1, Y - 1) in S):
+        recompute_s(image, S, X + 1, Y - 1)
+    if ((X + 1, Y) in S):
+        recompute_s(image, S, X + 1, Y)
+    if ((X + 1, Y + 1) in S):
+        recompute_s(image, S, X + 1, Y + 1)
+
+
+def create_s(image):
+    a = {}
+    for x in range(0, len(image)):
+        for y in range(0, len(image[x])):
+            if(image[x][y]):
+                a[(x,y)] = get_value(image, x - 1, y - 1) + get_value(image, x - 1, y) + get_value(image, x - 1, y + 1)\
+                    + get_value(image, x    , y - 1) + get_value(image, x    , y) + get_value(image, x    , y + 1) \
+                    + get_value(image, x + 1, y - 1) + get_value(image, x + 1, y) + get_value(image, x + 1, y + 1)
+    return a
+
+
+def dimitri_algo(x1, x2):
+    images = [x1.copy()]
+    x = x2 - x1
+
+    S = create_s(x)
+    while (len(S)):
+        d = max(S.iteritems(), key=operator.itemgetter(1))[0]
+        x1[d[0]][d[1]] += x[d[0]][d[1]]
+        images.append(x1.copy())
+        x[d[0]][d[1]] = 0
+        recompute_s_all(x, d, S)
+        del S[d]
+    return images
 
 
 def main():
     training_set, validation_set, test_set = mnist_loader.load_data()
     images = get_images(training_set)
-    images = [image for (image, digit) in images if digit == 5]
-    # images = [image for (image, digit) in images]
-    # idxs = random.sample(xrange(1, len(images)), 2)
+    images = [image for (image, digit) in images if digit == 4]
     x,y = get_max_entropy_images(images[0:200])
-    saveImage("x.png", images[x])
-    saveImage("y.png", images[y])
-    # ani_frame('demo.mp4', images[0:100])
+
+    saveImage("image1.png", images[x])
+    saveImage("image2.png", images[y])
+
+    images = dimitri_algo(images[x], images[y])
+    ani_frame('morphing.mp4', images)
 
 
 if __name__ == "__main__":
@@ -143,4 +183,3 @@ if __name__ == "__main__":
     # print(args.foo)
 
     main()
-    # test()
